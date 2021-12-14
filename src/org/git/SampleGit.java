e1c6a961222e73214ee13ba105a3884f0c8acb39
Jagadish0303 <jagaajraina@gmail.com>
2021-12-14 10:30:27 +0530

    Hello

---
+++ src/org/git/SampleGit.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class SampleGit {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Hello World");
+		System.out.println("Jagadish");
+		System.out.println("Good Morning");
+		System.out.println("12345");
+	}
+
+}
